5fb2e145cb18d0ed9cf623916d8408f13b5188ef
Zaid <karaymehzaid@gmail.com>
2022-06-01 15:05:40 +0300

    0.1.2 static files, settings config

--- backend/settings.py
+++ backend/settings.py
@@ -123,20 +123,20 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
 import os
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static'
+
 import psycopg2
 
-DATABASE_URL = os.environ['DATABASE_URL']
-
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-
-import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-
 if os.getcwd() == '/app':
+
+    DATABASE_URL = os.environ['DATABASE_URL']
+
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+    import dj_database_url
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
     # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     # SECURE_SSL_REDIRECT = True
     # DEBUG = False
-    pass
